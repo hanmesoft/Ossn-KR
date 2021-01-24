4065bb0bfae8a729a84fb8bb396f1700c173aa54
hanmesoft <76424448+hanmesoft@users.noreply.github.com>
2021-01-24 17:44:50 +0900

    Add files via upload

---
+++ ossn-v5.2/components/OssnMessages/locale/ossn.ko.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$ko = array(
+    'user:messages' => '쪽지',
+    'inbox' => '받은 쪽지',
+    'send' => '보내기',
+    'ossn:message:between' => '쪽지 %s',
+    'messages' => '쪽지',
+    'message:placeholder' => '여기에 입력하세요',
+    'no:messages' => '쪽지가 없습니다.',
+	'ossnmessages:deleted' => '삭제했습니다',
+);
+ossn_register_languages('ko', $ko); 
