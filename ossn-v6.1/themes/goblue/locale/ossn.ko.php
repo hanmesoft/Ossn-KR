18fde83e54f7378fd37be1cd4927e4bc2eb7d850
hanmesoft <76424448+hanmesoft@users.noreply.github.com>
2022-03-24 16:01:03 +0900

    Add files via upload

---
+++ ossn-v6.1/themes/goblue/locale/ossn.ko.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (openteknik.com).ossn
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$ko = array(
+	'goblue:settings' => 'GoBlue',
+	'admin:theme:goblue' => 'GoBlue',
+	'theme:goblue:logo:site' => '사이트 로고',
+	'theme:goblue:logo:admin' => '관리 로고',
+	'theme:goblue:logo:large' => '로고 파일이 너무 큼!',
+	'theme:goblue:logo:failed' => '로고 등록 실패',
+	'theme:goblue:logo:changed' => '로고를 바꾸었습니다.',
+	'theme:goblue:browercache' => '이미지가 보이지 않을때는 브라우저 캐시를 청소해 보세요.'
+);
+ossn_register_languages('ko', $ko);
